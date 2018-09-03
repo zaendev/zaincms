30f55aca210be973c736edd3018a3f31057aa7e9
zain <31380073+zainstar@users.noreply.github.com>
2018-09-03 21:09:24 +0700

    Update page.blade.php

--- resources/views/site/page.blade.php
+++ resources/views/site/page.blade.php
@@ -1,24 +1,17 @@
-@extends('home.master')
+@extends('site.master')
 @section('content')
 
-     @if( !empty($page->img) )
-         <div class="jumbotron jumbotron-category bg-img" style="background-image: url('{{ url('image/'.$page->img) }}')">
-     @else
-        <div class="jumbotron jumbotron-category">
-     @endif
-            <div class="container">
-                <h1 class="display-3 text-center">{{ $page->title }}</h1>
+    <div class="bg-header"></div>
+
+    <div class="container">
+        <div class="content">
+            <div class="bg-title-category">
+                <h1>{{ $data->title }}</h1>
+                <p>{!! $data->content !!}</p>
             </div>
-        </div>
+            <div class="content-blog">
 
-
-    <div class="container mb-5">
-        <div class="row">
-
-            <div class="col-md-12">
-                {!! $page->description !!}
             </div>
-
         </div>
     </div>
 
